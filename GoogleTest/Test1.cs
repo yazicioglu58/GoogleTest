62a4684c225a5901568bd6ebd2621b23d99c90d0
Mehmet Yazici <mehmetytx@gmail.com>
2022-02-23 22:21:52 -0600

    Updated herokuapp test

--- GoogleTest/Test1.cs
+++ GoogleTest/Test1.cs
@@ -1,29 +1,29 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GoogleTest
 {
-    
+
     public class HerokuTest
     {
         IWebDriver driver;
 
         public HerokuTest()
         {
-
-
         }
 
         [Test]
         public void GoogleTest2()
         {
-          driver = new ChromeDriver();
+            driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 
@@ -33,21 +33,23 @@ namespace GoogleTest
 
             IList<IWebElement> availableElements = driver.FindElements(By.XPath("//div[@id='content']//a"));
 
+           
             Console.WriteLine("Number of elements: " + availableElements.Count);
 
             Assert.IsTrue(availableElements.Count.Equals(expectedNumberOfElements));
-            
+            Thread.Sleep(3000);
+
             CloseDriver();
         }
 
         public void CloseDriver()
         {
-            if(driver != null)
+            if (driver != null)
             {
                 driver.Quit();
                 _ = driver == null;
-            }           
+            }
         }
-              
+
     }
 }
